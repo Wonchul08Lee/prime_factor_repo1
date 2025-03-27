44e7c7c9bd3f938e381b12ce2bfc36e5735eb807
wonchul08.lee <wonchul08.lee@gmail.com>
2025-03-27 15:22:07 +0900

    modify indent

--- PrimeFactor/primefactor.cpp
+++ PrimeFactor/primefactor.cpp
@@ -4,15 +4,14 @@ using namespace std;
 class PrimeFactor {
 public:
 	vector<int> of(int number) {
-		vector<int> result = {};
+	vector<int> result = {};
 
-		for (int divisor = 2; number > 1; divisor++) {
-			while (number % divisor == 0) {
-				result.push_back(divisor);
-				number /= divisor;
-			}
+	for (int divisor = 2; number > 1; divisor++) {
+		while (number % divisor == 0) {
+			result.push_back(divisor);
+			number /= divisor;
 		}
-
-		return result;
+	}
+	return result;
 	}
 };
