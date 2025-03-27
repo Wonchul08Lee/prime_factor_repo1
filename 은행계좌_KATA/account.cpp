10a07d6813ba0b4d687c1e6af30cce395c4e94c8
wonchul08.lee <wonchul08.lee@gmail.com>
2025-03-27 12:56:25 +0900

    TDD code commit

--- 은행계좌_KATA/account.cpp
+++ 은행계좌_KATA/account.cpp
@@ -1,4 +1,42 @@
 class Account{
 public:
+	Account(int money) : balance{ money } {}
+	int getBalance() {
+		return balance;
+	}
+	void deposit(int money) {
+		balance += money;
+	}
+	void withdraw(int money) {
+		balance -= money;
+	}
+	
+	void interest(int ratio_) {
+		calBalance(ratio_);
+	}
 
+	void applyinterest() {
+		calBalance(ratio);
+	}
+	void setInterest(int ratio_) {
+		ratio = ratio_;
+	}
+	int getIntereset() {
+		return ratio;
+	}
+	void calNYearBalance(int year) {
+		for (int y = 0; y < year; y++) {
+			applyinterest();
+		}
+	}
+	
+private:
+		int balance =0;
+		int ratio =0;
+
+		void calBalance(int ratio_)
+		{
+			balance += (balance * ratio_ / 100);
+		}
 };
+
--- 은행계좌_KATA/main.cpp
+++ 은행계좌_KATA/main.cpp
@@ -2,8 +2,59 @@
 #include "account.cpp"
 
 
-
+using namespace testing;
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
 }
+
+class AccountFixture : public Test {
+public:
+	Account account{ 10000 };
+
+};
+
+TEST_F(AccountFixture, CreateAccountInit10000won) {
+	
+	EXPECT_EQ(account.getBalance(), 10000);
+}
+
+TEST_F(AccountFixture, Deposit) {	
+	account.deposit(500);	
+	EXPECT_EQ(account.getBalance(), 10500);
+}
+
+TEST_F(AccountFixture, Withdraw) {	
+	account.withdraw(600);	
+	EXPECT_EQ(account.getBalance(), 9400);
+}
+
+TEST_F(AccountFixture, Interest) {
+	account.interest(5);
+	EXPECT_EQ(account.getBalance(), 10500);
+}
+
+TEST_F(AccountFixture, setInterest) {
+	account.setInterest(5);
+	EXPECT_EQ(account.getIntereset(), 5);
+}
+
+TEST_F(AccountFixture, setBalanceInterest) {
+	account.setInterest(5);
+	account.applyinterest();
+	EXPECT_EQ(account.getBalance(), 10500);
+}
+
+TEST_F(AccountFixture, getBalanceAfterNYear) {
+	account.setInterest(5);	
+
+	account.calNYearBalance(3);
+	int expected = 10000 + 10000 * 0.05 +
+		(10000 + 10000 * 0.05) * 0.05 +
+		(10000 + 10000 * 0.05 +
+			(10000 + 10000 * 0.05) * 0.05) * 0.05;
+		
+	EXPECT_EQ(account.getBalance(), expected);
+
+}
+
