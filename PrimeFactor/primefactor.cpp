4527a185547514330a71e2dbf0d85f56c4eaf613
wonchul08.lee <wonchul08.lee@gmail.com>
2025-03-27 14:43:30 +0900

    add more TCs

--- PrimeFactor/primefactor.cpp
+++ PrimeFactor/primefactor.cpp
@@ -5,30 +5,13 @@ class PrimeFactor {
 public:
 	vector<int> of(int number) {
 		vector<int> result = {};
-		if (number > 1) {
-			int divisor = 2;
-			if (number == 4) {
-				while (number % divisor == 0) {
-					result.push_back(divisor);
-					number /= divisor;
-				}
-			}
-			else if (number == 6) {
-				for (divisor = 2; number > 1; divisor++) {
-					while (number % divisor == 0) {
-						result.push_back(divisor);
-						number /= divisor;
-					}
-				}				
-			}
-			else if (number == 9) {
-				result.push_back(3);
-				result.push_back(3);
-			}
-			else {
-				result.push_back(number);
-			}
+	
+	for (int divisor = 2; number > 1; divisor++) {
+		while (number % divisor == 0) {
+			result.push_back(divisor);
+			number /= divisor;
 		}
+	}				
 			
 		return result;
 	}
